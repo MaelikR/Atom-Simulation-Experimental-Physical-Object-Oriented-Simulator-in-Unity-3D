3adf9f72b26cfca155d628bd61d639d738a7dab1
Light <57936434+MaelikR@users.noreply.github.com>
2025-03-27 03:07:29 +0100

    Create LiquidBody.cs

---
+++ LiquidBody.cs
@@ -0,0 +1,28 @@
+public class LiquidBody
+{
+    public float Diameter; // m
+    public float Height;   // m
+    public float Density;  // kg/m³
+    public Molecule MoleculeType;
+
+    public LiquidBody(float diameter, float height, float density, Molecule molecule)
+    {
+        Diameter = diameter;
+        Height = height;
+        Density = density;
+        MoleculeType = molecule;
+    }
+
+    public double GetTotalAtoms()
+    {
+        double radius = Diameter / 2.0;
+        double volume = Mathf.PI * Mathf.Pow((float)radius, 2) * Height;
+        double mass = volume * Density; // kg
+        double grams = mass * 1000;
+
+        double moles = grams / MoleculeType.MolarMass;
+        double molecules = moles * 6.022e23;
+
+        return molecules * MoleculeType.AtomCount;
+    }
+}
