3ed96ba5b965fb627d4fa81b8590b5313fe75b77
Light <57936434+MaelikR@users.noreply.github.com>
2025-03-27 04:10:22 +0100

    Update AtomSpawner.cs

--- Scripts/AtomSpawner.cs
+++ Scripts/AtomSpawner.cs
@@ -3,20 +3,13 @@ using Fusion;
 
 public class AtomSpawner : NetworkBehaviour
 {
-    public GameObject atomPrefabGO;
-    private NetworkPrefabRef atomPrefabRef;
-
-    public override void Spawned()
-    {
-        // Crée une référence NetworkPrefabRef à partir du GameObject enregistré
-        atomPrefabRef = Runner.GetPrefabRef(atomPrefabGO);
-    }
+    [SerializeField] private GameObject atomPrefab;
 
     public void SpawnAtom(Vector3 position, Vector3 initialVelocity)
     {
         if (HasStateAuthority)
         {
-            Runner.Spawn(atomPrefabRef, position, Quaternion.identity, Runner.LocalPlayer, 
+            Runner.Spawn(atomPrefab, position, Quaternion.identity, Runner.LocalPlayer, 
                 (runner, obj) => {
                     var atom = obj.GetComponent<Atom>();
                     atom.Velocity = initialVelocity;
