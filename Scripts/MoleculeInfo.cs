a879691c270a5de93446cc81b0646da0bb97cfa9
Light <57936434+MaelikR@users.noreply.github.com>
2025-04-04 03:19:37 +0200

    Create MoleculeInfo.cs

---
+++ Scripts/MoleculeInfo.cs
@@ -0,0 +1,7 @@
+using UnityEngine;
+
+public class MoleculeInfo : MonoBehaviour
+{
+    public string moleculeName = "Hydrogen Atom";
+    public string moleculeState = "Stable - Neutral Charge";
+}
