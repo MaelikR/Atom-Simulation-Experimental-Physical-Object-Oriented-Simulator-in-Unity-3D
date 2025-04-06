777e80a0338ac528a083142a8c863b9a8157445e
Light <57936434+MaelikR@users.noreply.github.com>
2025-04-06 22:55:02 +0200

    Delete Scripts directory

--- Scripts/AutoRotate.cs
+++
@@ -1,4 +0,0 @@
-public class AutoRotate : MonoBehaviour
-{
-    void Update() => transform.Rotate(Vector3.up * Time.deltaTime * 10f);
-}
--- Scripts/MoleculeInfo.cs
+++
@@ -1,7 +0,0 @@
-using UnityEngine;
-
-public class MoleculeInfo : MonoBehaviour
-{
-    public string moleculeName = "Hydrogen Atom";
-    public string moleculeState = "Stable - Neutral Charge";
-}
