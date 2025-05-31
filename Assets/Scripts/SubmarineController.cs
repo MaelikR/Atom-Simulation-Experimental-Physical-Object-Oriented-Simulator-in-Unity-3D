cb561ddb3ae999f768f18874c30272bc0323029e
Light <57936434+MaelikR@users.noreply.github.com>
2025-05-31 21:40:23 +0200

    Add files via upload

---
+++ Assets/Scripts/SubmarineController.cs
@@ -0,0 +1,34 @@
+﻿// Attach this script to the submarine prefab
+using UnityEngine;
+
+public class SubmarineController : MonoBehaviour
+{
+    [Header("Movement Settings")]
+    public float moveSpeed = 5f;
+    public float turnSpeed = 30f;
+    public float verticalSpeed = 2f;
+
+    [Header("Physics")]
+    public Rigidbody rb;
+
+    void Start()
+    {
+        if (rb == null) rb = GetComponent<Rigidbody>();
+        rb.useGravity = false;
+        rb.drag = 1.5f;
+    }
+
+    void FixedUpdate()
+    {
+        float moveForward = Input.GetAxis("Vertical") * moveSpeed;
+        float turn = Input.GetAxis("Horizontal") * turnSpeed;
+        float vertical = 0f;
+
+        if (Input.GetKey(KeyCode.Space)) vertical = verticalSpeed;
+        if (Input.GetKey(KeyCode.LeftControl)) vertical = -verticalSpeed;
+
+        Vector3 movement = transform.forward * moveForward + transform.up * vertical;
+        rb.AddForce(movement);
+        rb.MoveRotation(rb.rotation * Quaternion.Euler(Vector3.up * turn * Time.fixedDeltaTime));
+    }
+}
---
+++ Assets/Scripts/SubmarineInteractor.cs
@@ -0,0 +1,60 @@
+﻿using UnityEngine;
+using Fusion;
+
+public class SubmarineInteractor : NetworkBehaviour
+{
+    [Header("Interaction Settings")]
+    public float interactionRange = 3f;
+    public LayerMask submarineLayer;
+    public KeyCode interactionKey = KeyCode.E;
+
+    private Camera playerCam;
+    private bool isInSubmarine = false;
+    private GameObject currentSubmarine;
+
+    public GameObject playerAvatar;
+    public GameObject submarineCamera;
+    public GameObject submarineController;
+
+    public override void Spawned()
+    {
+        if (!Object.HasInputAuthority) return;
+        playerCam = Camera.main;
+    }
+
+    void Update()
+    {
+        if (!Object.HasInputAuthority || isInSubmarine) return;
+
+        if (Input.GetKeyDown(interactionKey))
+        {
+            TryEnterSubmarine();
+        }
+    }
+
+    void TryEnterSubmarine()
+    {
+        Ray ray = new Ray(playerCam.transform.position, playerCam.transform.forward);
+        if (Physics.Raycast(ray, out RaycastHit hit, interactionRange, submarineLayer))
+        {
+            currentSubmarine = hit.collider.gameObject;
+            EnterSubmarine();
+        }
+    }
+
+    void EnterSubmarine()
+    {
+        isInSubmarine = true;
+
+        if (playerAvatar != null)
+            playerAvatar.SetActive(false);
+
+        if (submarineController != null)
+            submarineController.SetActive(true);
+
+        if (submarineCamera != null)
+            submarineCamera.SetActive(true);
+
+        Debug.Log("Player has entered the submarine.");
+    }
+}
