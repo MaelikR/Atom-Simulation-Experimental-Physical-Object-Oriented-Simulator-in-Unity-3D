54b8069ae6115fc5ad9d8d56f53cf1f5fe4d8e41
Light <57936434+MaelikR@users.noreply.github.com>
2025-06-04 17:12:53 +0200

    Update SharkAtomSystem.cs

--- Assets/Scripts/SharkAtomSystem.cs
+++ Assets/Scripts/SharkAtomSystem.cs
@@ -7,9 +7,10 @@ public class SharkAtomSystem : MonoBehaviour
 
     void Start()
     {
-        composition.Add(new Molecule("Water (H2O)", 3, 18.015f));
-        composition.Add(new Molecule("Protein", 20000, 50000f));
-        composition.Add(new Molecule("Calcium", 20, 40.078f)); // pour les os/cartilage
+        composition.Add(new Molecule("Water (H2O)", 3, 18.015f, true, "H2O"));
+        composition.Add(new Molecule("Protein", 20000, 50000f, false, "C137H220N38O42")); // exemple de formule protéique
+        composition.Add(new Molecule("Calcium", 20, 40.078f, true, "Ca"));
+
     }
 
     public float GetTotalMolarMass()
